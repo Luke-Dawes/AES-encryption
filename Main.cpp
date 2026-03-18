5a26a4021141b7d1d242d13761da686d912df1bf
Luke Dawes <luke@daweshome.co.uk>
2026-03-18 15:08:48 +0000

    created 128 -> 4x32 function

---
+++ Main.cpp
@@ -0,0 +1,26 @@
+#include "Main.h"
+
+int main() {
+	
+}
+
+std::vector<bit32> split128to32(bit128 k) {
+	std::vector<bit32> w;
+
+	 
+	for (int j = 0; j < 4; ++j) {
+		bit32 ans = 0;
+		for (int i = 0; i < 32; ++i) {
+			k.test(j * 32 + i) ? ans.set(i) : ans.reset(i);
+		}
+		w.push_back(ans);
+	}
+	return w;
+}
+
+
+std::vector<bit128> keySchedule(bit128 k) {
+
+
+
+}
--- Main.h
+++ Main.h
@@ -1,1 +1,14 @@
 #pragma once
+#include <cstdint>
+#include <bitset>
+#include <vector>
+
+
+const int KEYSIZE = 128;
+const int BLOCKSIZE = 128;
+
+typedef std::bitset<128> bit128;
+typedef std::bitset<32> bit32;
+
+std::vector<bit128> keySchedule(bit128 key);
+std::vector<bit32> split128to32(bit128 key);
