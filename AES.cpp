5b3f0b9eb391a16c7f30bce058b1c92caa3442d3
Luke Dawes <luke@daweshome.co.uk>
2026-03-18 17:03:57 +0000

    fixed stupid error of () instead of []

--- AES.cpp
+++ AES.cpp
@@ -38,7 +38,7 @@ bit32 subWord(bit32 w) {
 	for (int i = 0; i < 4; ++i) {
 		unsigned char byte = (w >> (i * 8)).to_ulong() & 0xFF;
 
-		unsigned char substituted = sbox(byte);
+		unsigned char substituted = sbox[byte];
 
 		result |= (bit32(substituted) << (i * 8));
 	}
