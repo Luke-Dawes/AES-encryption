32be6d0d8d3027bfd2990f439a79ac08d8c92dca
Luke Dawes <luke@daweshome.co.uk>
2026-03-18 16:55:06 +0000

    created 32->128

--- AES.cpp
+++ AES.cpp
@@ -13,6 +13,19 @@ std::vector<bit32> split128to32(bit128 k) {
 	return w;
 }
 
+std::vector<bit128> join32to128(std::vector<bit32> w) {
+	std::vector<bit128> keys;
+
+	for (size_t i = 0; i < 44; i += 4) {
+		bit128 key;
+		key |= (bit128(w[i].to_ulong()) << 96);
+		key |= (bit128(w[i + 1].to_ulong()) << 64);
+		key |= (bit128(w[i + 2].to_ulong()) << 32);
+		key |= bit128(w[i + 3].to_ulong());
+		keys.push_back(key);
+	}
+	return keys;
+}
 
 
 bit32 rotWord(bit32 w) {
@@ -38,5 +51,5 @@ std::vector<bit128> keySchedule(bit128 k) {
 
 		w.push_back(w[i - 4] ^ temp);
 	}
-
+	
 }
--- AES.h
+++ AES.h
@@ -13,6 +13,7 @@ typedef std::bitset<32> bit32;
 //calculate KeySchedule
 std::vector<bit128> keySchedule(bit128 key);
 std::vector<bit32> split128to32(bit128 key);
+std::vector<bit128> join32to128(std::vector<bit32> w);
 std::vector<bit32> rotWord(std::vector<bit32> w);
 bit32 subWord(bit32 w);
 bit32 SBox(bit32 w);
