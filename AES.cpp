a2810df220a32e68f163f158091196ff9b79d2a9
Luke Dawes <luke@daweshome.co.uk>
2026-03-18 15:16:19 +0000

    changed file structure

---
+++ AES.cpp
@@ -0,0 +1,25 @@
+#include "AES.h"
+
+
+std::vector<bit32> split128to32(bit128 k) {
+	std::vector<bit32> w;
+
+
+	for (int j = 0; j < 4; ++j) {
+		bit32 ans = 0;
+		for (int i = 0; i < 32; ++i) {
+			k.test(j * 32 + i) ? ans.set(i) : ans.reset(i);
+		}
+		w.push_back(ans);
+	}
+	return w;
+}
+
+
+std::vector<bit128> keySchedule(bit128 k) {
+
+	std::vector<bit32> w = split128to32(k);
+
+
+
+}
